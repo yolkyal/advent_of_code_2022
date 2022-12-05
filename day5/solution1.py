b81243fc432f5f9a58184e46f902131fa5c3ff10
yolkyal <yolkyal@hotmail.co.uk>
2022-12-05 13:18:32 +0000

    Day 5 Solutions

---
+++ day5/solution1.py
@@ -0,0 +1,22 @@
+stacks = []
+for i in range(10):
+	stacks.append([])
+
+with open(0) as f:
+	while True:
+		line = next(f)
+		if line[1] == '1':
+			break
+		else:
+			for i, c in enumerate(line):
+				if c == '[':
+					stacks[(i // 4) + 1].append(line[i+1])
+	
+	next(f)
+	for line in f:
+		m, f, t = list(map(int, line.strip().replace('move', '').replace('from', '').replace('to', '').split()))
+		for i in range(m):
+			stacks[t] = [stacks[f].pop(0)] + stacks[t]
+	
+	result = ''.join([stack[0] for stack in stacks if len(stack) > 0])
+	print(result)
